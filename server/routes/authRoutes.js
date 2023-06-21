29af28546c2182d561b9daa85e8b796d4a126345
Sandeepmauryaworld <85479357+sandeepmauryaworld@users.noreply.github.com>
2023-06-21 14:40:56 +0530

    add middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
   registerController,
   loginController,
+  currentUserController,
 } = require("../controller/authController");
+const authMiddelware = require("../middlewares/authMiddelware");
 
 const router = express.Router();
 
@@ -14,5 +16,8 @@ router.post("/register", registerController);
 // login || POST
 
 router.post("/login", loginController);
+
+// get current user || GET
+router.get("/current-user", authMiddelware, currentUserController);
 
 module.exports = router;
